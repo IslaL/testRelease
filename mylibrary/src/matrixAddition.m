21d5b4c117241c8c222af3fbaa41265ab31f0f94
Isla Li <islali0904@gmail.com>
2024-04-23 09:53:10 -0700

    testAutoRelease

--- mylibrary/src/matrixAddition.m
+++ mylibrary/src/matrixAddition.m
@@ -1,5 +1,5 @@
-function result = matrixAddition(a, b)
+function result = matrixAddition()
     %ADDITION sum of two matrices or vectors
     % this function returns the sum of a and b
-    result = a + b;
+    result = 10+20;
 end
---
+++ mylibrary/src/myCompare.m
@@ -0,0 +1,7 @@
+function result = myCompare(a, b)
+    if a > b
+        result = 0;
+    else
+        result = 1;
+    end
+end
